c0dd072385ae048295872e7612d58e4f84f2d83d
AKW <2497744746@qq.com>
2023-06-02 17:46:40 +0800

    ts 类型问题

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,20 @@
 import SvgIcon from "./SygIcon/index.vue";
 import { Component, DefineComponent } from "vue";
-import App from "@/App.vue";
+// import App from "@/App.vue";
 
 interface GlobalComponents {
   [key: string]: Component & DefineComponent<any, any, any>;
 }
-
+//
 const allGlobalComponent: GlobalComponents = { SvgIcon };
+export default allGlobalComponent;
 
-// 对外暴露插件对象
-export default {
-  // 必须叫install方法
-  install(app: App<Element>) {
-    Object.keys(allGlobalComponent).forEach((key) => {
-      app.component(key, allGlobalComponent[key]);
-    });
-  },
-};
+// // 对外暴露插件对象
+// export default {
+//   // 必须叫install方法
+//   install(app: App<Element>) {
+//     Object.keys(allGlobalComponent).forEach((key) => {
+//       app.component(key, allGlobalComponent[key]);
+//     });
+//   },
+// };
--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,17 @@ import "@/styles/index.scss";
 import router from "./router";
 import pinia from "./store";
 
-import globalComponent from "@/components/index";
+// import globalComponent from "@/components/index";
+import allGlobalComponent from "@/components";
 const app = createApp(App);
 
 app.use(Antd);
-app.use(globalComponent);
+// app.use(globalComponent);
 app.use(router);
 app.use(pinia);
 
 app.mount("#app");
+
+Object.keys(allGlobalComponent).forEach((key) => {
+  app.component(key, allGlobalComponent[key]);
+});
